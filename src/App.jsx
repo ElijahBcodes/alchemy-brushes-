19800860862e1d2e575ddbc196bce834c39eef91
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2023-10-23 19:54:11 -0700

    Added tailwindcss and daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+<div className="carousel rounded-box">
+  <div className="carousel-item">
+    <img src="https://images.unsplash.com/photo-1697949314141-584b03d2ecd5?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8" alt="Burger" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://images.unsplash.com/photo-1697806959316-4864f26a09cf?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D" alt="Burger" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://plus.unsplash.com/premium_photo-1666941941118-8b9ba7cd4685?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="Burger" />
+  </div> 
+  <div className="carousel-item">
+    <img src="https://images.unsplash.com/photo-1682687220336-bbd659a734e7?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D" alt="Burger" />
+  </div> 
+  <div className="carousel-item">
+    <img src="/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
+  </div> 
+  <div className="carousel-item">
+    <img src="/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
+  </div> 
+  <div className="carousel-item">
+    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+  </div>
+</div>
     </>
   )
 }
